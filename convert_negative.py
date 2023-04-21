aeb3f2f5ada75ba1b57cebad60e72b8be14dfc59
Zhuoyang <60202079+georgedashen@users.noreply.github.com>
2023-04-21 11:54:28 +0800

    Add files via upload

---
+++ convert_negative.py
@@ -0,0 +1,52 @@
+import numpy as np
+from tqdm import tqdm
+import pickle
+import random
+
+N = 500
+# N = 29148 for all items
+random.seed(123)
+
+# load train
+with open('train_user_consumed.pkl', 'rb') as f:
+    train = pickle.load(f)
+
+# construct train user-item matrix
+user_items_matrix = np.zeros((22976, 29148))
+for i, (user, items) in enumerate(train.items()):
+    for item in items:
+        user_items_matrix[int(user)][item] = 1
+
+#load val
+with open('val_user_consumed.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+#val_negative: generate N items including both pos and neg samples
+with open(f'val_negative_{N}', 'w') as f:
+    for i, (x, y_list) in enumerate(tqdm(data.items())):
+        y_str = ','.join(str(y) for y in y_list) # positive samples
+        N_rest = N - len(y_list) # number of negative samples
+        values = [i for i in range(29148) if i not in y_list and user_items_matrix[x][i]==0]
+        random.shuffle(values) # randomized
+        values = values[:N_rest] #negative sampling
+        for value in values:
+            user_items_matrix[x][value] = 1 # update user-item matrix
+        values_str = '\t'.join(str(i) for i in values)
+        line = f"({x},{y_str})\t{values_str}\n"
+        f.write(line)
+
+#load val
+with open('test_user_consumed.pkl', 'rb') as f:
+    data = pickle.load(f)
+    
+#test_negative: generate N items including both pos and neg samples
+with open(f'test_negative_{N}', 'w') as f:
+    for i, (x, y_list) in enumerate(tqdm(data.items())):
+        y_str = ','.join(str(y) for y in y_list) # positive samples
+        N_rest = N - len(y_list) # number of negative samples
+        values = [i for i in range(29148) if i not in y_list and user_items_matrix[x][i]==0]
+        random.shuffle(values) # randomized
+        values = values[:N_rest] #negative sampling
+        values_str = '\t'.join(str(i) for i in values)
+        line = f"({x},{y_str})\t{values_str}\n"
+        f.write(line)
